7ff52bea83b83c93e56e1d0fa76b405d0b67b4e5
Sylsee <sylvio.poliart@gmail.com>
2021-09-11 10:27:36 +0200

    fdf

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 06:34:59 by spoliart          #+#    #+#             */
-/*   Updated: 2021/09/11 10:05:21 by spoliart         ###   ########.fr       */
+/*   Updated: 2021/09/11 10:27:34 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,34 @@ static double	get_z(t_dot a, t_dot b, t_dot c)
 
 int	get_color(t_dot a, t_dot b, t_dot c, t_env *env)
 {
+	int		new[3];
+	int		new_color;
+	double	increment[3];
 	double	z;
-	int	color;
 
 	z = get_z(a, b, c);
-	if (z <= 0)
-	{
-		color =  - z;
-		color << 8 = 0xFFFFFF - z;
-		color << 16 = 0xFFFFFF - z;
-	}
-	else
-	{
-		color = 0xFFFFFF - z;
-		color << 8 = 0xFFFFFF - z;
-		color << 16 = 0xFFFFFF - z;
-	}
-	if (env->colorise == 2 && !z)
-		color = 0;
-	return (color);
+	increment[0] = (double)((R(env->end_color)) - (R(env->start_color))) /
+					(double)(a.);
+	increment[1] = (double)((G(env->end_color)) - (G(env->start_color))) /
+					(double)len;
+	increment[2] = (double)((B(env->end_color)) - (B(env->start_color))) /
+					(double)len;
+
+	new[0] = (R(env->start_color)) + ft_round(pix * increment[0]);
+	new[1] = (G(env->start_color)) + ft_round(pix * increment[1]);
+	new[2] = (B(env->start_color)) + ft_round(pix * increment[2]);
+
+	new_color = RGB(new[0], new[1], new[2]);
+	return (new_color);
 }
+/*
+   double	z;
+   int		color[3];
+
+   z = get_z(a, b, c);
+   color[1] =  - z;
+   color[2] = 0xFFFFFF - z;
+   color[2] = 0xFFFFFF - z;
+   if (env->colorise == 2 && !z)
+   color = 0;
+   return (color);*/
